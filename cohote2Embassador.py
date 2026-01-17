a800b096ccf6b2cdd527a0b68e374b8aa2d03cbe
Olivier  rishi M. <olivierrishimata@gmail.com>
2026-01-17 11:24:23 +0200

    embassador 2

---
+++ cohote2Embassador.py
@@ -0,0 +1,22 @@
+"""
+Midnight Ambassador – Community Script
+Author: Olivier Rishi Matabaro
+Role: Midnight Ambassador
+"""
+
+def midnight_ambassador_info():
+    print("🌙 Midnight Ambassador Program – Cohort 2")
+    print("-" * 50)
+    print("Privacy is not optional. It’s foundational.")
+    print("Midnight is building the privacy layer of Web3.\n")
+
+    print("📌 Want to become a Midnight Ambassador?")
+    print("Apply here 👉 https://mpc.midnight.network/midnight-ambassador\n")
+
+    print("📝 Referral information:")
+    print("Referred by: Olivierrishimatabaro\n")
+
+    print("🚀 Get in early. Help shape the future of private blockchain.")
+
+if __name__ == "__main__":
+    midnight_ambassador_info()
